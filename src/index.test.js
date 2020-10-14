677fd76f24faab29485695fd97aa08c80753234e
Sebastian Flor <sflor@telecomboutique.com>
2020-10-14 16:40:31 +0200

    unit test with mocha and chai

---
+++ src/index.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var expect = require('chai').expect;
+var names = require('.');
+
+describe('random-names', function(){
+    it('should have a list of all available names', function() {
+        expect(names.all).to.satisfy(isArrayOfStrings);
+      });
+    
+      it('should allow me to get a random name from the list', function() {
+        expect(names.random()).to.satisfy(isIncludedIn(names.all));
+      });
+});
+
+function isArrayOfStrings(array) {
+    return array.every(function(i) {
+      return typeof i === 'string';
+    });
+  }
+  
+  function isIncludedIn(array) {
+    return function(item) {
+      return array.includes(item);
+    };
+  }
